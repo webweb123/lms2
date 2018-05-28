ff8deaff15384ab731b1de48d84aaf13416a245a
hb <hb@hb6020>
2018-05-28 14:36:13 +0900

    2018-05-28/14:36

--- project1/src/com/bit/ctr/J/J_stu_list_controller.java
+++ project1/src/com/bit/ctr/J/J_stu_list_controller.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bit.model.Grade_check_Dao;
+import com.bit.model.Grade_check_Dto;
 import com.bit.model.Student_Dao;
 import com.bit.model.Student_Dto;
 
@@ -15,6 +17,8 @@ public class J_stu_list_controller extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Student_Dao a = new Student_Dao();
 		Student_Dto b = new Student_Dto();
+		Grade_check_Dao c = new Grade_check_Dao();
+		Grade_check_Dto d = new Grade_check_Dto();
 		
 	}
 	@Override
