3c2521faa114847e743f312e156d11e8e784d70f
hb <hb@hb6020>
2018-05-28 17:57:04 +0900

    dd

--- project1/WebContent/WEB-INF/sql/test.sql
+++ project1/WebContent/WEB-INF/sql/test.sql
@@ -3,4 +3,6 @@ insert into STU_MEMBER values(1,'test1','test1','test1','test1','test1','test1',
 insert into APP_BBS_STU values(1,'test1','test1',sysdate,'test1','test1','test1','test1','test1',1);
 insert into LEC_CREATE values(1,1,1,'test1',sysdate,sysdate,'test1',1);
 insert into STUDENT values(1,1,'test1','test1','test1','test1','test1','test1','test1',1);
+
+
 insert into FREE_BBS values(free_bbs_sequ.nextval,1,'test1','test1','test1',0,sysdate);
